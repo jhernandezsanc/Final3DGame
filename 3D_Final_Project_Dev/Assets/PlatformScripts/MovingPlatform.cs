22e1a040d1b36579bb58250afcdc5d5b371339d9
wpace1062-beep <wpace1062@sdsu.edu>
2026-05-08 19:15:29 -0700

    Level1FirstCommit
    duplicated level prototype, added walls, trampolines w/ textures, moving platforms

--- 3D_Final_Project_Dev/Assets/PlatformScripts/MovingPlatform.cs
+++ 3D_Final_Project_Dev/Assets/PlatformScripts/MovingPlatform.cs
@@ -7,14 +7,17 @@ public class MovingPlatform : MonoBehaviour
     public float speed = 2f;
 
     [Header("Detection Settings")]
-    public Vector3 detectionOffset = new Vector3(0, 1.1f, 0);
-    public Vector3 detectionSize = new Vector3(2f, 1f, 2f);
+    public Vector3 detectionOffset = new Vector3(0, 0.5f, 0);
+    public Vector3 detectionSize = new Vector3(10f, 0.5f, 10f);
+    public ParticleSystem thrusterFX;
 
     private Vector3 startPos;
     private Vector3 lastPos;
     private Vector3 platformDelta;
 
     private bool playerOnPlatform;
+    private Vector3 lastVelocity;
+    private float speedValue;
 
     void Start()
     {
@@ -25,8 +28,13 @@ public class MovingPlatform : MonoBehaviour
     void Update()
     {
         // 1. Move platform
-        float movement = Mathf.PingPong(Time.time * speed, moveDistance);
-        transform.position = startPos + moveDirection.normalized * movement;
+        float cycle = Time.time * speed;
+
+        float sine = Mathf.Sin(cycle);
+        float t = (sine + 1f) * 0.5f;
+
+        Vector3 targetPos = startPos + moveDirection.normalized * (t * moveDistance);
+        transform.position = targetPos;
 
         // 2. Calculate delta movement
         platformDelta = transform.position - lastPos;
@@ -40,6 +48,22 @@ public class MovingPlatform : MonoBehaviour
         {
             MasterMovement.Instance.frameDisplacement += platformDelta;
         }
+
+        //thrusterFX scripts
+        Vector3 velocity = (transform.position - lastPos) / Time.deltaTime;
+        speedValue = velocity.magnitude;
+
+        if (thrusterFX != null)
+        {
+            var emission = thrusterFX.emission;
+            emission.rateOverTime = Mathf.Lerp(0f, 60f, speedValue / 3f);
+
+            if (velocity.sqrMagnitude > 0.0001f)
+            {
+                thrusterFX.transform.rotation = Quaternion.LookRotation(-velocity.normalized);
+            }
+        }
+        lastPos = transform.position;
     }
 
     bool CheckForPlayer()
